c70e01641be40b1273cb6f3972c8d0440f293147
KeithMurph <keithmurphy94@gmail.com>
2021-07-26 21:39:21 -0700

    seeds.sql values

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE managment_db
 CREATE TABLE roles (
     id INT,
     job_title VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
+    roles_department VARCHAR(30) NOT NULL,
 
 );
 
@@ -21,9 +21,9 @@ CREATE TABLE employee (
     id INT, 
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    job_title VARCHAR(50) NOT NULL,
-    department VARCHAR(50) NOT NULL,
+    employee_job_title VARCHAR(50) NOT NULL,
+    employee_department VARCHAR(50) NOT NULL,
     salary INT NOT NULL,
-    manager VARCHAR(50) NOT NULL
+    employee_manager VARCHAR(50) NOT NULL
 
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+USE managment_db
+
+
+INSERT INTO roles (job_title, roles_department)
+VALUES  (""),
+        (""),
+        (""),
+        (""),
+        ("");
+
+INSERT INTO department (Id, deparment_name)
+VALUES  (1, ""),
+        (2, ""),
+        (3, ""),
+        (4, ""),
+        (5, "");
+
+INSERT INTO employee (id, first_name, last_name, employee_job_title,
+ employee_department, salary, employee_manager)
+VALUES  (1, ""),
+        (2, ""),
+        (3, ""),
+        (4, ""),
+        (5, "");
+
+
+
+        
